95dc28b9d0b708af0c284d24d591b62e584cb51d
rikza30 <3103120198@student.smktelkom-pwt.sch.id>
2022-07-18 10:44:16 +0700

    6

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 <body>
     <div id="app">
@@ -76,6 +78,8 @@
         </nav>
 
         <main class="py-4">
+            <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
             @yield('content')
         </main>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,4 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'home'])->name(
 
 Auth::routes();
 
-
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
