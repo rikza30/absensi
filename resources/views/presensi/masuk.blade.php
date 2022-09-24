99a091d4ac607c2c275b1403d0c0542645e35bd8
rikza30 <3103120198@student.smktelkom-pwt.sch.id>
2022-09-24 09:28:30 +0700

    t

--- resources/views/presensi/masuk.blade.php
+++ resources/views/presensi/masuk.blade.php
@@ -54,7 +54,7 @@
           <img src="{{asset('asset/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">{{Auth::user()->name}}</a>
+          <a href="home" class="d-block">{{Auth::user()->name}}</a>
         </div>
       </div>
 
